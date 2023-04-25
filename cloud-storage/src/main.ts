10987f1c920b9c9eb149ed0201c94d72717efd51
Aleksey Elchin <alonso85@mail.ru>
2023-04-26 01:32:35 +0900

    nest js

--- cloud-storage/src/main.ts
+++ cloud-storage/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
+  });
+  app.enableCors({ credentials: true, origin: true });
+
+  await app.listen(7777);
 }
 bootstrap();
